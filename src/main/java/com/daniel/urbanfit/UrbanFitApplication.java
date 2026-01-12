fe9a86c20f849ecb5d03266eac78ea719253633c
Daniel <Daniel@TelePCA6>
2026-01-12 18:35:02 +0100

    Inicio del Proyecto UrbanFit

---
+++ src/main/java/com/daniel/urbanfit/UrbanFitApplication.java
@@ -0,0 +1,13 @@
+package com.daniel.urbanfit;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class UrbanFitApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(UrbanFitApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/daniel/urbanfit/UrbanFitApplicationTests.java
@@ -0,0 +1,13 @@
+package com.daniel.urbanfit;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class UrbanFitApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
